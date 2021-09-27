f70fa972d669affcfb82bb4b69e3a938deee6018
Armen <armenzg@sentry.io>
2021-09-27 14:55:22 -0400

    Update uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,9 @@
 # It is used in CI to get the Github workers into a closer state
 # as new laptops come in.
 # This can also be used in development to test executions of bootstrap.sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-[ -f /usr/local/bin/chromedriver ] && rm /usr/local/bin/chromedriver
+[ -f /Applications/Docker.app ] && rm -rf /Applications/Docker.app
+# Since we install pre-commit via brew
+[ -f .git/hooks/pre-commit ] && rm .git/hooks/pre-commit
+if command -v brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+fi
