8317132e4d0727bee13f2ad695da9d21532de846
Joshua Li <joshuarli98@gmail.com>
2023-08-10 10:31:18 -0700

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -5,16 +5,13 @@
 # This script tries to remove as much as reasonable to allow for re-testing
 # bootstrap.sh multiple times on the same host
 
-remove_brew_setup() {
-    # Removes all packages
-    brew list -1 | xargs brew rm
-    # Execute the official uninstall command
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-    # This is not removed by the script.
-    [ -d /opt/homebrew ] && sudo rm -rf /opt/homebrew
-}
+if [ -n "$CI" ]; then
+  # macos in GHA is intel-only, so make sure we remove its brew
+  sudo rm -rf /usr/local/bin/brew
+fi
 
-command -v brew &>/dev/null && remove_brew_setup
+sudo rm -rf /opt/homebrew
+
 rm -rf ~/.pyenv
 rm -rf ~/.sentry
 rm -rf ~/code/sentry/{.venv,node_modules}
